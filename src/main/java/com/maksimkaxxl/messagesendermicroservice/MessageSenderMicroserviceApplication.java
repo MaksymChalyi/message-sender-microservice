1fbf44a24c8ed5bcaad9c8852bb7053caf76c1d7
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-06-07 01:00:41 +0300

    git init

---
+++ src/main/java/com/maksimkaxxl/messagesendermicroservice/MessageSenderMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.messagesendermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSenderMicroserviceApplication.class, args);
+    }
+
+}
